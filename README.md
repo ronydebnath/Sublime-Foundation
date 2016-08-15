5e2f77d20c0d6be039f8042d92a143ce0d9f164d
Rony Debnath <rony.cse09@gmail.com>
2016-08-15 14:09:01 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+
+
+# Zurb Foundation 6
+
+Foundation is a family of responsive front-end frameworks that make it easy to design beautiful responsive websites, apps and emails that look amazing on any device. Foundation is semantic, readable, flexible, and completely customizable. 
+
+This package allows to quickly open a Zurb Foundation project from Sublime Text. 
+
+## Installation
+
+**Package Control**
+
+Select 'Foundation' from the Install Package... menu
+
+**Vanilla Sublime**
+
+Clone this repo into your Packages folder and Rename folder name 'Sublime-Foundation-master' to 'Foundation'.
+
+## Usage
+
+Just run the command:
+
+```
+Foundation: New Project
+```
+
+Provide a path for your new project and hit enter. That's it!
+
+**Note:**  
+If you're using Sublime Text 2, there's no way to set the current project programmatically, so you'll have to select the new .sublime-project file from the dialog that appears.
+
+## Settings
+
+- `zip_url`: The url to a customized build from <http://foundation.zurb.com/sites/download.html/> with the skeleton you want to proceed.
+
+- `default_path`: The location that will be pre-filled in the dialog box on creation.
+ 
+## License
+
+BSD3
+
+## Acknowledgment
+
+Since, i use Zurb Foundation instead of HTML Boilerplate for most of my projects, So i made this plugin which is inspired from Initializr plugin made by [Dan Drinkard].
+
+[Dan Drinkard]: <https://github.com/drinks>
---
+++ foundation.py
@@ -0,0 +1,91 @@
+import sublime, sublime_plugin
+import glob
+import json
+import os
+import sys
+import shutil
+import errno
+
+try:
+    from urllib.request import urlopen as open_url
+except:
+    from urllib import urlopen as open_url
+
+from io import BytesIO
+from zipfile import ZipFile
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+
+class FoundationProject(sublime_plugin.WindowCommand):
+    zf = None
+    settings = None
+
+    def run(self):
+        self.settings = sublime.load_settings('foundation.sublime-settings')
+        self.window.show_input_panel('Where to save your new project?',
+                                     self.settings.get('default_path', '[your-path-to-localhost]/projectName'),
+                                     self.create, None, None)
+
+    def create(self, path):
+        if path.startswith('~/'):
+            path = os.path.expanduser(path)
+        path = os.path.realpath(path)
+        if os.path.exists(path):
+            self.window.show_input_panel('That path exists! Where to save your new project?',
+                                         self.settings.get('default_path', '[your-path-to-localhost]/projectName'),
+                                         self.create, None, None)
+            return
+        self.download()
+        if not self.verify():
+            sublime.error_message('Zipfile contained a suspicious path. Aborting...')
+            return
+
+        mkdir_p(path)
+        self.zf.extractall(path)
+        try:
+            for fn in glob.glob('%s/foundation/*' % path):
+                shutil.move(fn, '%s/' % path)
+            os.rmdir('%s/foundation' % path)
+        except:
+            pass
+
+        self.finish(path)
+
+    def download(self):
+        url = self.settings.get('zip_url')
+        try:
+            self.zf = ZipFile(BytesIO(open_url(url).read()))
+        # For ST2 Compat
+        except AttributeError:
+            self.zf = ZipFile(BytesIO(open_url(url).read()))
+
+    def verify(self):
+        for name in self.zf.namelist():
+            if '../' in name or name.startswith('/'):
+                return False
+        return True
+
+    def finish(self, path):
+        project_data = {
+            'folders': [{
+                'follow_symlinks': True,
+                'path': path,
+            }]
+        }
+        try:
+            sublime.run_command('new_window')
+            sublime.active_window().set_project_data(project_data)
+        except:
+            name = os.path.split(path)[1]
+            proj = '%s/%s.sublime-project' % (path, name)
+            json.dump(project_data, open(proj, 'w+'))
+            sublime.active_window().run_command('close')
+            sublime.run_command('prompt_open_project')
